96825868c0a808bed39d8fa393e76090a34508ac
Carlos Ruesta <carlos@dcide.com.br>
2021-02-28 21:52:07 -0300

    Aula 01: Entities e Value Objects

---
+++ src/Email.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+// Value Objects, pois não precisa ter um identificador.
+// É o Aluno que tem identificação, mas não o Email
+class Email
+{
+    private string $endereco;
+
+    public function __construct(string $endereco)
+    {
+        if (filter_var($endereco, FILTER_VALIDATE_EMAIL) === false) {
+            throw new \InvalidArgumentException("Endereço de e-mail inválido");
+        }
+        $this->endereco = $endereco;
+    }
+
+    public function __toString()
+    {
+        return $this->endereco;
+    }
+}
