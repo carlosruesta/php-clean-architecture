ad9c8a931cd38ef68a85a97b6cef3280ba75d736
Carlos Ruesta <carlos@dcide.com.br>
2021-03-21 18:20:16 -0300

    Aula 05: Implementando a Criptografia de Senhas: Interface no dominio e Implementacoes na Infra para utilizar qualquer uma

---
+++ src/Infra/Aluno/CriptografadorDeSenhaMd5.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Alura\Arquitetura\Infra\Aluno;
+
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\CriptografadorDeSenha;
+
+class CriptografadorDeSenhaMd5 implements CriptografadorDeSenha
+{
+
+    public function criptografarSenha(string $senhaEmTexto): string
+    {
+        return password_hash($senhaEmTexto, PASSWORD_ARGON2ID);
+    }
+
+    public function senhaEhValida(string $senhaEmTexto, Aluno $aluno): bool
+    {
+        return password_verify($senhaEmTexto, $aluno->senha());
+    }
+}
